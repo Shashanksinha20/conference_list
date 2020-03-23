451b2d9f0cd2780e1fc7de4e0de6bd94afa1a752
Shashank Sinha <shashank@Shashanks-MacBook-Pro.local>
2020-03-23 13:38:21 +0530

    Updates

---
+++ precache-manifest.da1bc30fe2b72b48ce508e9f2725c250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61c071e78fa4f02bb161ebe0dc6ff0f",
+    "url": "/Shashanksinha20/conference_list/index.html"
+  },
+  {
+    "revision": "a5deec5b2f2d79d16681",
+    "url": "/Shashanksinha20/conference_list/static/css/main.cb7cf3ff.chunk.css"
+  },
+  {
+    "revision": "3bbb82c3a1a78561192d",
+    "url": "/Shashanksinha20/conference_list/static/js/2.2541c21b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shashanksinha20/conference_list/static/js/2.2541c21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5deec5b2f2d79d16681",
+    "url": "/Shashanksinha20/conference_list/static/js/main.92885775.chunk.js"
+  },
+  {
+    "revision": "2e595f04e43b1945f590",
+    "url": "/Shashanksinha20/conference_list/static/js/runtime-main.e18e2bce.js"
+  }
+]);
